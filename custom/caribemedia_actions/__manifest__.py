a8a00935a62c320dc7ffa6ae00c599a67cc60485
anagparedes <anagparedesg@gmail.com>
2024-01-20 19:42:23 -0400

    [REF] caribe_media_actions: refactor module

---
+++ custom/caribemedia_actions/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'caribemedia_actions',
+    'version': '15.0.1.0.0',
+    'depends': ['base', 'contacts'],
+    'author': 'Caribe Media',
+    'description': 'Add Server Actions for Contacts',
+   'data': [
+        'views/contact_views.xml',
+        'data/contact_actions.xml',
+    ],
+}
