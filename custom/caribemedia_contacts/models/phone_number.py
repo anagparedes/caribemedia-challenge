230382fc645c119bf40139727311eac00b288560
anagparedes <anagparedesg@gmail.com>
2024-01-20 19:43:39 -0400

    [REF] caribe_media_contacts: refactor module

--- custom/caribemedia_contacts/models/phone_number.py
+++ custom/caribemedia_contacts/models/phone_number.py
@@ -1,14 +1,14 @@
-from odoo import api, models, fields
+from odoo import models, fields
 
 class PhoneNumber(models.Model):
     _name = 'phone.number'
     _description = 'Phone Numbers'
 
-    partner_id = fields.Many2one('res.partner', string='Contact', ondelete='cascade', readonly=True)
-    phone_number = fields.Char(string='Phone', required=True, default=lambda self:self.partner_id.phone)
-    raw_phone = fields.Char(string='Raw Phone', compute='_compute_raw_phone')
-    is_active = fields.Boolean(string='Active', default=True)
-    is_main_phone = fields.Boolean(string='Main Phone')
+    partner_id = fields.Many2one('res.partner', string='Contact', ondelete='cascade', readonly=True, store=True)
+    phone_number = fields.Char(string='Phone', required=True, default=lambda self:self.partner_id.phone, store=True)
+    raw_phone = fields.Char(string='Raw Phone', compute='_compute_raw_phone', store=True)
+    is_active = fields.Boolean(string='Active', default=True, store=True)
+    is_main_phone = fields.Boolean(string='Main Phone', store=True)
 
     def _compute_raw_phone(self):
         for rec in self:
