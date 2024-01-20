747b99c6cd0da15561808f5c41eb2ec52f3149da
anagparedes <anagparedesg@gmail.com>
2024-01-20 19:07:41 -0400

    [ADD] caribemedia_stock: add a search filter for sale price in the Stock

---
+++ custom/caribemedia_stock/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'caribe_media_stock',
+    'version': '15.0.1.0.0',
+    'depends': ['stock'],
+    'author': 'Caribe Media',
+    'category': 'Warehouse',
+    'description': """
+        Add a search filter for sale price in the Stock.
+    """,
+    'data': [
+        'views/product_template.xml',
+    ],
+}
